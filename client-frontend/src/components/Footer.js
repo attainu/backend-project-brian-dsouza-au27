b6d6e3bcbbb826a894b60714189a12f7572e7d06
Brian Dsouza <91792234+brian-dsouza-au27@users.noreply.github.com>
2022-05-05 08:44:33 +0530

    Add files via upload

---
+++ client-frontend/src/components/Footer.js
@@ -0,0 +1,96 @@
+// import React from "react";
+
+// const Footer = () => {
+//   return (
+//     <div className="footer">
+//       <div className="justify-content-center d-flex">
+//         <div className="card-name">
+//           <img
+//             alt="mastercard"
+//             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/1200px-MasterCard_Logo.svg.png"
+//           />
+//         </div>
+//         <div className="card-name">
+//           <img
+//             alt="visa"
+//             src="https://upload.wikimedia.org/wikipedia/commons/4/41/Visa_Logo.png"
+//           />
+//         </div>
+//         <div className="card-name">
+//           <img
+//             alt="paypal"
+//             src="https://pbs.twimg.com/media/EfTZlEnWAAMn1lX.png"
+//           />
+//         </div>
+//         <div className="card-name">
+//           <img
+//             alt="express"
+//             src="https://icons.iconarchive.com/icons/designbolts/credit-card-payment/256/American-Express-icon.png"
+//           />
+//         </div>
+//         <div className="card-name">
+//           <img
+//             alt="discover"
+//             src="https://icons-for-free.com/iconfiles/png/512/cash+checkout+discover+network+online+shopping+payment+method-1320191225548835050.png"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="black" className="footer">
+      <div className="mai">
+      <h3 id="devi">NOOB Developer.....</h3>
+        <p>
+        Hey Divi Nation! Thanks for joining us , we give away brand new freebies. Recently, we shared a brand new Shoe Repair Layout Pack. To help you get your website  running as soon as possible, we’re sharing a global header & footer template that matches this layout pack perfectly as well! Hope you enjoy it.
+        </p>
+        <div id="all">
+
+        <div className="card-name">
+          <img
+            alt="mastercard"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/1200px-MasterCard_Logo.svg.png"
+            />
+        </div>
+        <div className="card-name">
+          <img
+            alt="visa"
+            src="https://upload.wikimedia.org/wikipedia/commons/4/41/Visa_Logo.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt="paypal"
+            src="https://pbs.twimg.com/media/EfTZlEnWAAMn1lX.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt="express"
+            src="https://icons.iconarchive.com/icons/designbolts/credit-card-payment/256/American-Express-icon.png"
+            />
+        </div>
+        <div className="card-name">
+          <img
+            alt="discover"
+            src="https://icons-for-free.com/iconfiles/png/512/cash+checkout+discover+network+online+shopping+payment+method-1320191225548835050.png"
+            />
+        </div>
+      </div>
+        <div className="cname">
+        Copyright © 2022 ShoeShop ®
+        </div>
+            </div>
+    </div>
+  );
+};
+
+export default Footer;
